9b1885648e7f6802a69b9dc9d8d97b01cefd0466
TheGnewGuy <john.gnew@verizon.net>
2014-12-29 20:03:15 -0600

    XML

--- ODSUtilities/ODSUtilities.cpp
+++ ODSUtilities/ODSUtilities.cpp
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include "unzip.h"
 #include "zip.h"
+#include "..\tinyxml\tinyxml.h"
 
 TCHAR mBuff[1024];		// Buffer for messages
 
@@ -51,6 +52,154 @@ void ExtractToMemory(HZIP hz, int index, ZIPENTRY ze)
 	delete[] ibuf;
 }
 
+void processXMLFile()
+{
+	TiXmlDocument doc("content.xml");
+	bool loadOkay = doc.LoadFile();
+
+	if (!loadOkay)
+	{
+		printf("Could not load test file 'content.xml'. Error='%s'. Exiting.\n", doc.ErrorDesc());
+		exit(1);
+	}
+
+	printf("** Demo doc read from disk: ** \n\n");
+	printf("** Printing via doc.Print **\n");
+	doc.Print(stdout);
+
+	//{
+	//	printf("** Printing via TiXmlPrinter **\n");
+	//	TiXmlPrinter printer;
+	//	doc.Accept(&printer);
+	//	fprintf(stdout, "%s", printer.CStr());
+	//}
+
+	TiXmlNode* node = 0;
+	TiXmlElement* todoElement = 0;
+	TiXmlElement* itemElement = 0;
+
+}
+
+
+// ----------------------------------------------------------------------
+// STDOUT dump and indenting utility functions
+// ----------------------------------------------------------------------
+const unsigned int NUM_INDENTS_PER_SPACE = 2;
+
+const char * getIndent(unsigned int numIndents)
+{
+	static const char * pINDENT = "                                      + ";
+	static const unsigned int LENGTH = strlen(pINDENT);
+	unsigned int n = numIndents*NUM_INDENTS_PER_SPACE;
+	if (n > LENGTH) n = LENGTH;
+
+	return &pINDENT[LENGTH - n];
+}
+
+// same as getIndent but no "+" at the end
+const char * getIndentAlt(unsigned int numIndents)
+{
+	static const char * pINDENT = "                                        ";
+	static const unsigned int LENGTH = strlen(pINDENT);
+	unsigned int n = numIndents*NUM_INDENTS_PER_SPACE;
+	if (n > LENGTH) n = LENGTH;
+
+	return &pINDENT[LENGTH - n];
+}
+
+int dump_attribs_to_stdout(TiXmlElement* pElement, unsigned int indent)
+{
+	if (!pElement) return 0;
+
+	TiXmlAttribute* pAttrib = pElement->FirstAttribute();
+	int i = 0;
+	int ival;
+	double dval;
+	const char* pIndent = getIndent(indent);
+	printf("\n");
+	while (pAttrib)
+	{
+		printf("%s%s: value=[%s]", pIndent, pAttrib->Name(), pAttrib->Value());
+
+		if (pAttrib->QueryIntValue(&ival) == TIXML_SUCCESS)    printf(" int=%d", ival);
+		if (pAttrib->QueryDoubleValue(&dval) == TIXML_SUCCESS) printf(" d=%1.1f", dval);
+		printf("\n");
+		i++;
+		pAttrib = pAttrib->Next();
+	}
+	return i;
+}
+
+void dump_to_stdout(TiXmlNode* pParent, unsigned int indent = 0)
+{
+	if (!pParent) return;
+
+	TiXmlNode* pChild;
+	TiXmlText* pText;
+	int t = pParent->Type();
+	printf("%s", getIndent(indent));
+	int num;
+
+	switch (t)
+	{
+	case TiXmlNode::TINYXML_DOCUMENT:
+		printf("Document");
+		break;
+
+	case TiXmlNode::TINYXML_ELEMENT:
+		printf("Element [%s]", pParent->Value());
+		num = dump_attribs_to_stdout(pParent->ToElement(), indent + 1);
+		switch (num)
+		{
+		case 0:  printf(" (No attributes)"); break;
+		case 1:  printf("%s1 attribute", getIndentAlt(indent)); break;
+		default: printf("%s%d attributes", getIndentAlt(indent), num); break;
+		}
+		break;
+
+	case TiXmlNode::TINYXML_COMMENT:
+		printf("Comment: [%s]", pParent->Value());
+		break;
+
+	case TiXmlNode::TINYXML_UNKNOWN:
+		printf("Unknown");
+		break;
+
+	case TiXmlNode::TINYXML_TEXT:
+		pText = pParent->ToText();
+		printf("Text: [%s]", pText->Value());
+		break;
+
+	case TiXmlNode::TINYXML_DECLARATION:
+		printf("Declaration");
+		break;
+	default:
+		break;
+	}
+	printf("\n");
+	for (pChild = pParent->FirstChild(); pChild != 0; pChild = pChild->NextSibling())
+	{
+		dump_to_stdout(pChild, indent + 1);
+	}
+}
+
+// load the named file and dump its structure to STDOUT
+void dump_to_stdout(const char* pFilename)
+{
+	TiXmlDocument doc(pFilename);
+	bool loadOkay = doc.LoadFile();
+	if (loadOkay)
+	{
+		printf("\n%s:\n", pFilename);
+		dump_to_stdout(&doc); // defined later in the tutorial
+	}
+	else
+	{
+		printf("Failed to load file \"%s\"\n", pFilename);
+	}
+}
+
+
 int _tmain(int argc, _TCHAR* argv[])
 {
 	printf("This program shows how to open an existing zipfile, display information about file.\n");
@@ -80,6 +229,8 @@ int _tmain(int argc, _TCHAR* argv[])
 		case ZR_OK:
 			//ExtractToMemory(hz, i, ze);
 			ExtractToDisk(hz, i, ze);
+			processXMLFile();
+			dump_to_stdout("content.xml");
 			break;
 		case ZR_NOTFOUND:
 			msg(_T("* Failed to find xcontent.xml"));
